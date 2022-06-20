1679acfeb88ef2372d2acc683a0f3ea75b812cc6
Daniel Perkowski <daniel.perkowski.991@gmail.com>
2022-06-20 20:47:06 +0200

    Add user Model

---
+++ User.cs
@@ -0,0 +1,14 @@
+namespace food_book;
+
+public class User
+{
+    private int id {get; set;}
+
+    private DateTime createdAcc { get; set; }
+
+    public string name  { get; set; }
+
+    public string mail { get; set; }
+
+    public string hashPass  { get; set; }
+}
