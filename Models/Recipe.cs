4ac3821f424c65c7b3ce8898bf18e46109129a59
Daniel Perkowski <daniel.perkowski.991@gmail.com>
2022-06-21 18:40:50 +0200

    Create Recipe Model

---
+++ Models/Recipe.cs
@@ -0,0 +1,49 @@
+namespace food_book;
+
+public class Recipe
+{
+    public string name { get; set; }
+
+    public List<int> categories { get; set; }
+
+    public int hardLevel { get; set; }
+
+    public int time { get; set; }
+}
+
+public class UserRecipe : Recipe
+{
+    public long userId { get; set; }
+    
+    public bool userFavorite { get; set; }
+
+    public void Edit(string name, UserRecipe editedRecipe)
+    {
+        // Edit by name
+    }
+
+    public void Remove(string name)
+    {
+        // Remove from db
+    }
+
+    public void ToggleFav()
+    {
+        userFavorite = !userFavorite;
+    }
+}
+
+public class ExternalRecipe : Recipe
+{
+    private bool isOnUserList { get; set; }
+    
+    public string source { get; set; }
+
+    public void MoveToUser(long userId, bool isFavorite)
+    {
+        isOnUserList = true;
+        
+        // Add in db to userRecepies
+        
+    }
+}
