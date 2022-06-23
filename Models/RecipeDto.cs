5c110b6d3c97b64d1810b995c31f7e8099fb88fa
Daniel Perkowski <daniel.perkowski.991@gmail.com>
2022-06-23 19:02:36 +0200

    Add Full CRUD for UserRecipe

--- Models/RecipeDto.cs
+++ Models/RecipeDto.cs
@@ -1,4 +1,6 @@
-﻿namespace food_book;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace food_book;
 
 public class RecipeAddDto
 {
@@ -15,4 +17,30 @@ public class RecipeAddDto
     public long userId { get; set; }
     
     public bool userFavorite { get; set; }
+}
+
+public class RecipeEditDto
+{
+    public long id { get; set; }
+    
+    public string name { get; set; }
+    
+    public string desc { get; set; }
+    
+    public int hardLevel { get; set; }
+
+    public int time { get; set; }
+    
+    public string image { get; set; }
+    
+    public long userId { get; set; }
+    
+    public bool userFavorite { get; set; }
+}
+
+public class UserRecipeResponseDto
+{
+    public ActionResult<List<UserRecipe>> UserRecipes { get; set; }
+    
+    public string message { get; set; }
 }
