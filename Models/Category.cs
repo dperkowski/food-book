4610067e231fcd20f4452987d96f19cfa0c8166c
Daniel Perkowski <daniel.perkowski.991@gmail.com>
2022-07-03 17:15:51 +0200

    Add Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace food_book;
+
+public class Category
+{
+    [Key]
+    public long id {get; set;}
+    
+    public string name { get; set; }
+    
+}
