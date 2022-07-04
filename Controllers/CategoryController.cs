3da8428b4638f3c5543e128c5f74ee0d4274757d
Daniel Perkowski <daniel.perkowski.991@gmail.com>
2022-07-04 19:33:49 +0200

    Add Category Controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using food_book.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace food_book.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly DataContext _context;
+
+    public CategoryController(IConfiguration configuration, DataContext context)
+    {
+        _configuration = configuration;
+        _context = context;
+    }
+    
+    [HttpGet("getAll")]
+    public async Task<ActionResult<List<Category>>> GetAll()
+    {
+        return Ok(await _context.Categories.ToListAsync());
+    }
+    
+}
