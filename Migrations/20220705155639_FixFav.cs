a0a13ad3468e96996832740f22ff99e9678ed15a
Daniel Perkowski <daniel.perkowski.991@gmail.com>
2022-07-05 17:57:04 +0200

    Fav Fix Migration

---
+++ Migrations/20220705155639_FixFav.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace food_book.Migrations
+{
+    public partial class FixFav : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
